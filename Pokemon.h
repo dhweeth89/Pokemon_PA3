4ed889181831e4dd4c8b743c82ffc115cce44c7a
dhweeth89 <sgupta89@bu.edu>
2019-11-08 10:51:52 -0500

    Made Pokemon header file

---
+++ Pokemon.h
@@ -0,0 +1,66 @@
+#include "Point2D.h"
+#include "Vector2D.h"
+#include "GameObject.h"
+#include "PokemonCenter.h"
+#include "PokemonGym.h"
+#include <iostream>
+
+using namespace std;
+
+class Pokemon{
+
+    enum PokemonStates{
+        STOPPED = 0,
+        MOVING = 1,
+        EXHAUSTED = 2,
+        IN_GYM = 3,
+        IN_CENTER = 4,
+        MOVING_TO_GYM = 5,
+        MOVING_TO_CENTER = 6,
+        TRAINING_IN_GYM = 7,
+        RECOVERING_STAMINA = 8
+
+    };
+
+    private:
+
+        double speed;
+        bool is_in_gym;
+        bool is_in_center;
+        unsigned int stamina;
+        unsigned int experience_points;
+        double pokemon_dollars;
+        unsigned int training_units_to_buy;
+        unsigned int stamina_points_to_buy;
+        string name;
+        PokemonCenter* current_center;
+        PokemonGym* current_gym;
+        Point2D destination;
+        Vector2D delta;
+
+    protected:
+
+        bool UpdateLocation();
+        void SetupDestination(Point2D);
+
+    public:
+
+        Pokemon();
+        Pokemon(char);
+        Pokemon(string, int, char, unsigned int, Point2D);
+        
+        void StartMoving(Point2D);
+        void StartMovingToCenter(PokemonCenter*);
+        void StartMovingToGym(PokemonGym*);
+        void StartTraining(unsigned int);
+        void StartRecoveringStamina(unsigned int);
+        void Stop();
+        bool IsExhausted();
+        bool ShouldBeVisible();
+        void ShowStatus();
+        bool Updaate();
+
+};
+
+
+double GetRandomAountOfPokemonDollars();
